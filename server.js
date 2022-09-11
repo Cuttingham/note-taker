ca0f2e7861633692df88545598c901ba7f0b46bc
Jacob Karla <jacob.l.karla@gmail.com>
2022-09-11 12:26:38 -0400

    created server in server.js file, should be working

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
+const app = express();
+
+const PORT = process.env.PORT ||3000;
+
+app.use(express.urlencoded ({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./Develop/routes/routes');
+
+app.listen(PORT,function(){
+    console.log('app listened on port: '+PORT);
+})
